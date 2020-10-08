601062f6f4da6af8d45fe58d9029fe1cbdff3b09
HeidiAT <71800562+HeidiAT@users.noreply.github.com>
2020-10-08 12:13:15 +0800

    Add files via upload

---
+++ Aritmatika.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Aritmatika
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Operasi Aritamtika"+"Pada Tipe Integer");
+		int a = 2 + 1;
+		int b = a - 1;
+		int c = a * b;
+		int d = c / 3;
+		int e = -a;
+		System.out.println("Nilai a= "+a);
+		System.out.println("Nilai b= "+b);
+		System.out.println("Nilai c= "+c);
+		System.out.println("Nilai d= "+d);
+		System.out.println("Nilai e= "+e);
+		System.out.println();
+	}
+}
